5b7479fd1482a228170cf87282602284c1031ffa
SumathishJain <sumathishjain.2002@gmail.com>
2025-02-17 22:20:03 +0530

    commit

--- browser_commands.py
+++ browser_commands.py
@@ -30,23 +30,3 @@ driver.refresh()
 print('window refreshed')
 time.sleep(3)
 driver.close()
-
-
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import time
-
-# driver = webdriver.Chrome()
-# driver.get("https://opensource-demo.orangehrmlive.com/")
-# driver.maximize_window()
-
-# # Wait for the element to be clickable before clicking
-# wait = WebDriverWait(driver, 10)
-# forgot_password = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "p.oxd-text.oxd-text--p.orangehrm-login-forgot-header")))
-# forgot_password.click()
-
-# print("Clicked on Forgot Password")
-# time.sleep(3)
-# driver.quit()
